3310c1beeb368e017a51f07312fc959f95b79f95
ernaniraffo <ernani.raffo@icloud.com>
2023-03-04 23:18:44 -0800

    cleaner print

--- vectorTest.py
+++ vectorTest.py
@@ -1,10 +1,10 @@
 from vector import Vector3D
 
 a = Vector3D(2, 4, 6)
-print("a:", a)
+print("a =", a)
 
 b = Vector3D(5, 2, 1)
-print("b:", b)
+print("b =", b)
 
 a = a + b
 print("a = a + b =", a)
