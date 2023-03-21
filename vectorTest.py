356072fe2830aa6063be2b71aef6fce4ca620fa9
ernaniraffo <ernani.raffo@icloud.com>
2023-03-21 15:17:43 -0700

    cleaner tests

--- vectorTest.py
+++ vectorTest.py
@@ -1,4 +1,5 @@
 from vector import Vector3D
+from typing import List, Callable
 from colorama import Fore, Style
 
 
@@ -22,25 +23,59 @@ def vec_add_test() -> int:
         return 2
     return 0
 
+def vec_dot_test() -> int:
+    a = Vector3D(1, 1, 1)
+    b = Vector3D(2, 2, 2)
+    res = a.dot(b)
+    if (res != 6):
+        return 1
+    a.x, a.y, a.z = 80, 20, 100
+    res = a * b
+    if (res != 400):
+        return 2
+    b.x, b.y, b.z = -1, -1, -1
+    res = a * b
+    if (res != -200):
+        return 3
+    return 0
+
+def vec_cross_test() -> int:
+    a = Vector3D(50, 50, 50)
+    b = Vector3D(-100, 2, 0)
+    c = a.cross(b)
+    if (c.x != -100 or c.y != -5000 or c.z != 5100):
+        return 1
+    a.x, a.y, a.z = -50, -50, -50
+    c = a.cross(b)
+    if (c.x != 100 or c.y != 5000 or c.z != -5100):
+        return 2
+    c = b.cross(a)
+    if (c.x != -100 or c.y != -5000 or c.z != 5100):
+        return 3
+    c = a.cross(a)
+    if (c.x != 0 or c.y != 0 or c.z != 0):
+        return 4
+    c = b.cross(b)
+    if (c.x != 0 or c.y != 0 or c.z != 0):
+        return 5
+    return 0
+
+def test(functions: List[Callable]) -> None:
+    for f in functions:
+        try:
+            test_case = f()
+            if (test_case):
+                raise RuntimeError("Test " + str(test_case))
+            print(f.__name__ + ":", Fore.GREEN + "PASSED")
+        except Exception as e:
+            print("Error in", f.__name__ + ":", str(e) + ":", Fore.RED + "FAIL")
+        print(Style.RESET_ALL, end='')
+            
 def main():
     print("Starting tests...")
-    
-    try:
-        t = vec_create_test()
-        if (t != 0):
-            raise RuntimeError("Test " + str(t))
-        print("vec_create_test:", Fore.GREEN + "PASSED" + Style.RESET_ALL)
-    except Exception as e:
-        print("Error in vec_create_test:", str(e) + ":", Fore.RED + "FAIL")
-    
-    try:
-        t = vec_add_test()
-        if (t != 0):
-            raise RuntimeError("Test " + str(t))
-        print("vec_add_test:", Fore.GREEN + "PASSED")
-    except Exception as e:
-        print("Error in vec_add_test:", str(e) + ":", Fore.RED, "FAIL" + Style.RESET_ALL)
-
+    functions = [vec_add_test, vec_create_test, vec_dot_test, vec_cross_test]
+    test(functions)
+    print("Finished")
 
 if __name__ == "__main__":
     main()
