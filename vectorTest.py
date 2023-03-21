86b6d9cfaff9d84ffb9e2beff03e5091c80dbf46
ernaniraffo <ernani.raffo@icloud.com>
2023-03-21 00:44:12 -0700

    unit tests rather than program

--- vectorTest.py
+++ vectorTest.py
@@ -1,21 +1,46 @@
 from vector import Vector3D
+from colorama import Fore, Style
 
-a = Vector3D(2, 4, 6)
-print("a =", a)
 
-b = Vector3D(5, 2, 1)
-print("b =", b)
+def vec_create_test() -> int:
+    a = Vector3D(2, 4, 6)
+    b = Vector3D(5, 2, 1)
+    if (a.x != 2 or a.y != 4 or a.z != 6):
+        return 1
+    if (b.x != 5 or b.y != 2 or b.z != 1):
+        return 2
+    return 0
 
-a = a + b
-print("a = a + b =", a)
+def vec_add_test() -> int:
+    a = Vector3D(-2, 4, -90)
+    b = Vector3D(18, 2, -2)
+    c = a + b
+    if (c.x != 16 or c.y != 6 or c.z != -92):
+        return 1
+    a += b
+    if (a.x != c.x or a.y != c.y or a.z != c.z):
+        return 2
+    return 0
 
-print("b · a =", b * a)
-print("b - a=", b - a)
+def main():
+    print("Starting tests...")
+    
+    try:
+        t = vec_create_test()
+        if (t != 0):
+            raise RuntimeError("Test " + str(t))
+        print("vec_create_test:", Fore.GREEN + "PASSED" + Style.RESET_ALL)
+    except Exception as e:
+        print("Error in vec_create_test:", str(e) + ":", Fore.RED + "FAIL")
+    
+    try:
+        t = vec_add_test()
+        if (t != 0):
+            raise RuntimeError("Test " + str(t))
+        print("vec_add_test:", Fore.GREEN + "PASSED")
+    except Exception as e:
+        print("Error in vec_add_test:", str(e) + ":", Fore.RED, "FAIL" + Style.RESET_ALL)
 
-print("a.length() =", a.length())
-a.normalize()
-print("normalize(a) =", a)
 
-c = a.projection(b)
-print("c = a.projection(b) =", c)
-
+if __name__ == "__main__":
+    main()
